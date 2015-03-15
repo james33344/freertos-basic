c6f9f3a059daf1c3d57427a9278c7f4fd0a50ce6
james33344 <james455096@gmail.com>
2015-03-15 12:34:30 +0800

    Add Fibonacci test function.

--- src/testfunc.c
+++ src/testfunc.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include "testfunc.h"
+#include "clib.h"
+#include "fio.h"
+#include "FreeRTOS.h"
 
 int fib(int number){
+	if(number>40 || number <0){
+		fio_printf(2, "Input error, see man page.\n");
+		return -1;		
+	}
 
+	int previous = -1;
+	int result = 1;
+	int sum = 0;
+	int i = 0;
 
-	return 0;
+	for(i=0; i<=number; i++){
+		sum = result + previous;
+		previous = result;
+		result = sum;
+		fio_printf(1, "Fibonacci %d is %d.\n",i , sum);
+	}
+	return 1;
 }
 
