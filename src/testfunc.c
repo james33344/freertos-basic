21c2c98bf1767bf7cc12f7a633ff5ce7d43eb473
james33344 <james455096@gmail.com>
2015-04-01 23:01:05 +0800

    Add `get_time` to calculate execute time.     `get_time` comes from https://github.com/james33344/visualizer/main.c Add semihost API to prepare for testing on real HW.

--- src/testfunc.c
+++ src/testfunc.c
@@ -4,9 +4,19 @@
 #include "fio.h"
 #include "FreeRTOS.h"
 #include "timers.h"
+#include "host.h"
 extern int fibonacci(int);
 static int count = 0;
+ unsigned int get_time()
+ {
+     static unsigned int const *reload = (void *) 0xE000E014;
+     static unsigned int const *current = (void *) 0xE000E018;
+     static const unsigned int scale = 1000000 / configTICK_RATE_HZ;
+                     /* microsecond */
 
+     return xTaskGetTickCount() * scale +
+            (*reload - *current) / (*reload / scale);
+}
 void fib_timer(xTimerHandle pxTimer){
 	count++;
 }
@@ -16,7 +26,9 @@ int fib(int number){
 		fio_printf(2, "Input error, see man page.\n\r");
 		return -1;		
 	}
-
+	int t;
+	char buf_echo[128] = "echo fibonacci ";
+	char* buf;
 	int result = 0;
 /*	xTimerHandle t;
 	t = xTimerCreate((const signed char*)"timer",
@@ -25,11 +37,18 @@ int fib(int number){
 						NULL,
 						fib_timer
 						);	
-
 	xTimerStart(t,0);*/
+	t = get_time();
 	result = fibonacci(number);
+	t = get_time() - t;
+	buf = itoa("0123456789",number,10);
+	strcat(buf_echo,buf);
+	strcat(buf_echo," takes time is: ");
+	buf = itoa("0123456789",t,10);
+	strcat(buf_echo,buf);
 //	xTimerStop(t,0);
-	fio_printf(1,"The fibonacii sequence %d is %d. Exec ticks is %d\n\r",number,result,count);
+	host_action(SYS_SYSTEM, buf_echo);
+	fio_printf(1,"The fibonacii sequence %d is %d. Exec ticks is %d\n\r",number,result,t);
 	
 	return 1;
 }
